1642bda54dc2f00b72ad275bb228eabc3afba0a3
Ryan Gorup <gorup@users.noreply.github.com>
2017-09-01 19:37:04 -0700

    Move to interview directoy/package, rename Order to GetBuildOrder, create stub of MarkovChain

---
+++ src/com/gorup/interview/MarkovChain.java
@@ -0,0 +1,8 @@
+package com.gorup.interview;
+
+public class MarkovChain {
+    public static void main(String[] args) {
+        final String target = "a";
+        System.out.println(String.format("Build order for %s", target));
+    }
+}
